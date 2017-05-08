90143d8009c59439403879ff43f137a88c2b4c14
Fabio da Costa Anastacio <fabiowfabio@yahoo.com.br>
2017-05-08 10:32:11 -0300

    Atualização do programa.

--- so-exercicio-24-04-2017/src/Exemplo2.java
+++ so-exercicio-24-04-2017/src/Exemplo2.java
@@ -5,23 +5,33 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class Exemplo1 {
+public class Exemplo2 {
 
 	public static void main(String[] args) throws IOException {
 		
 		Scanner entrada = new Scanner(new FileInputStream(FileDescriptor.in));
 		
 		PrintWriter saida = new PrintWriter(new FileOutputStream(FileDescriptor.out));
+
+		PrintWriter arquivo = new PrintWriter("src\\filmes.txt");
 		
-		saida.println("Digite o seu nome: ");
+		saida.println("Queremos saber seus 4 filmes favoritos");
 		saida.flush();
-		String nome = entrada.nextLine();
-		saida.println("Prazer em conhecê-lo, " + nome + "!");
+		
+		for(int i = 0; i < 4; i++) {
+			saida.print("Nome do filme: ");
+			saida.flush();
+			String nome = entrada.nextLine();
+			arquivo.println(nome);
+		}
+		
+		saida.println("Obrigado por participar da pesquisa!");
+		saida.println("Os dados foram gravados em src\\filmes.txt");
 		saida.flush();
 		
 		entrada.close();
 		saida.close();
-		
+		arquivo.close();
 	}
-	
+
 }
